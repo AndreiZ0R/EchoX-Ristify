8160eab730ad920af7d99930563123fd4d101635
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-10-30 22:19:35 +0200

    Spring Boot + PostgreSQL project init

---
+++ backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/RistifyBackendApplication.java
@@ -0,0 +1,11 @@
+package com.ristify.ristifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistifyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RistifyBackendApplication.class, args);
+    }
+}
---
+++ backend/Ristify-Backend/src/test/java/com/ristify/ristifybackend/RistifyBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ristify.ristifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest
+class RistifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true, equalTo(true));
+    }
+
+}
