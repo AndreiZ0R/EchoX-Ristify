1438997e2a45e6effd486542fb1cc43585e70c7c
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-11-05 22:07:36 +0200

    configured cors

--- backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/RistifyBackendApplication.java
+++ backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/RistifyBackendApplication.java
@@ -2,10 +2,26 @@ package com.ristify.ristifybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RistifyBackendApplication {
+    private final static String VITE_DEFAULT_HOST = "http://localhost:5173";
+
     public static void main(String[] args) {
         SpringApplication.run(RistifyBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(VITE_DEFAULT_HOST);
+            }
+        };
     }
 }
