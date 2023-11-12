a29c853df01dd03902b5f62f8d42d60087cfa7fa
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-11-12 22:21:29 +0200

    User Service & Controller - WIP

--- backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/utils/AppUtils.java
+++ backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/utils/AppUtils.java
@@ -9,4 +9,20 @@ public class AppUtils {
     public static final String PAYLOAD = "payload";
     public static final String SUCCESS = "Success";
     public static final String HOST = "http://localhost:";
+
+    public static String constructNotFoundMessage(Class<?> clazz, final String propertyName, final Object value) {
+        return "Could not find " + clazz.getSimpleName() + " with " + propertyName + ": " + value;
+    }
+
+    public static String constructFailedSaveMessage(final Class<?> clazz) {
+        return "Could not save " + clazz.getSimpleName();
+    }
+
+    public static String constructFailedDeleteMessage(final Class<?> clazz, final Object object) {
+        return "Failed to delete " + clazz.getSimpleName() + ": " + object;
+    }
+
+    public static String constructSuccessDeleteMessage(final Class<?> clazz, final Object id) {
+        return "Successfully deleted " + clazz.getSimpleName() + ": " + id;
+    }
 }
