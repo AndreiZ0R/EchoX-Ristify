9d57956e5952f91729b76d87f1a45fa7f2f40fc7
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-12-04 10:21:35 +0200

    [FE] Migrate to typescript react (#9)
    * migrate to typescript
    
    * -

--- frontend/Ristify-Frontend/src/App.tsx
+++ frontend/Ristify-Frontend/src/App.tsx
@@ -4,15 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState<number>(0)
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
                 </a>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                <a href="https://react.dev" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
@@ -22,7 +22,7 @@ function App() {
                     count is {count}
                 </button>
                 <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
+                    Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">
