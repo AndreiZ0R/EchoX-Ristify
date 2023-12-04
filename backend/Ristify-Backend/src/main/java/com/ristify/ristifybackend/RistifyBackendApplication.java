b4791fd3dd9dd05506934b40cb334ee4f8f5ad66
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-12-04 08:45:48 +0200

    main refactor

--- backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/RistifyBackendApplication.java
+++ backend/Ristify-Backend/src/main/java/com/ristify/ristifybackend/RistifyBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition
 public class RistifyBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RistifyBackendApplication.class, args);
     }
